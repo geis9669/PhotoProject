221e9870ead7150e6430d5d8e3fd4f7a535a2e9a
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-01-29 12:40:13 -0700

    got this to find and then be able to call the found methods of the pricture project

--- src/pixLab/classes/PictureMyFunTests.java
+++ src/pixLab/classes/PictureMyFunTests.java
@@ -5,18 +5,71 @@ import java.util.*;
 
 public class PictureMyFunTests
 {
-    String classs = "Picture";
-
+    private static String classs = "pixLab.classes.Picture";
+    private static final Picture originalPicture = new Picture("beach.jpg");
+    
     public static void main(String[] args)
     {
+        List<Method> methodsToCall = getMethods();
+        printMethods(methodsToCall);
+        
+        originalPicture.explore();
+        
+        List<List<Method>> combinationMethods ;
+        
+        
+//        callMethods(methodsToCall);
+        
+        //List<Method> methodCombinations = methodsToCall;
         
     }
     
+    private static List<List<Method>> getCombinations(List<Method> list)
+    {
+        List<List<Method>> results = new ArrayList<>();
+        
+        
+        
+        return results;
+    }
+    
+    
+
+    private static void callMethods(List<Method> methodsToCall)
+    {
+        for(int index = 0; index<methodsToCall.size(); index++)
+        {
+            try
+            {
+                Picture myPicture = new Picture(originalPicture.getHeight(),originalPicture.getWidth());
+                myPicture.copy(originalPicture, 0, 0);
+                
+                methodsToCall.get(index).invoke(myPicture);
+                myPicture.explore();              
+            }
+            catch(Exception e)
+            {
+                displayMessage(e.getMessage());
+            }
+        
+        }
+    }
+    
+    /*
+     * this will print all thats in the list to the display
+     */
+    private static void printMethods(List<Method> list)
+    {
+        for(Method print:list)
+        {
+            displayMessage(print.getName());
+        }
+    }
+    
     //extract all the methods that I want to do
-    private String[] getMethods()
+    private static List<Method> getMethods()
     {
         List<Method> toReturn = new ArrayList<>() ;
-       
         try
         {
             Class cl = Class.forName(classs);
@@ -26,20 +79,24 @@ public class PictureMyFunTests
             for(Method command: methods)
             {
                 String commandName = command.getName();
-                if(commandName.contains("Red")||commandName.contains("Blue")||commandName.contains("Green"))
+                if((commandName.contains("Red")||commandName.contains("Blue")||commandName.contains("Green"))
+                        && (commandName.contains("max") || commandName.contains("zero")))
                 {
-                    
+                    toReturn.add(command);
                 }
             }
-            
-            
         }
         catch(ClassNotFoundException exception)
         {
             exception.printStackTrace();
         }
         
-        
-        return null;
+        return toReturn;
+    }
+    
+    
+    private static void displayMessage(String message)
+    {
+        System.out.println(message);
     }
 }
