19528d63c0d1b8e2d2924fc07e1f498a91a1adb9
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-09-06 13:28:55 -0600

    added a class to load a second image when I need it for as a parameter

---
+++ src/pixLab/gui/questionsPopup/PictureParameter.java
@@ -0,0 +1,59 @@
+package pixLab.gui.questionsPopup;
+
+import java.io.IOException;
+
+import javax.swing.*;
+
+import pixLab.classes.Picture;
+
+public class PictureParameter extends ParameterInfo<JLabel, JComboBox<String>> {
+
+	public PictureParameter(String question, String[] list) {
+		super(createLabel(question), createJComboBox(list));
+	}
+	
+	public PictureParameter(String question, DefaultComboBoxModel<String> model)
+	{
+		super(createLabel(question), createJComboBox(model));
+	}
+	
+	private static JLabel createLabel(String message)
+	{
+		JLabel label = new JLabel(message);
+		return label;
+	}
+	
+	private static JComboBox<String> createJComboBox(DefaultComboBoxModel<String> model)
+	{
+		JComboBox<String> comboList = new JComboBox<>();
+		comboList.setModel(model);
+		return comboList;
+	}
+	
+	private static JComboBox<String> createJComboBox(String[] list)
+	{
+		JComboBox<String> comboList = new JComboBox<>();
+		DefaultComboBoxModel<String> modelList = new DefaultComboBoxModel<>(list);
+		comboList.setModel(modelList);
+		return comboList;
+	}
+
+	@Override
+	public boolean hasValidInput() {
+		return true;
+	}
+
+	@Override
+	public Object getValidatedInput() {
+		String pictureName = (String) getAnswerSpace().getSelectedItem();
+		Picture picture = new Picture(pictureName);
+		return picture;
+	}
+
+	@Override
+	public void clearAnswerSpace() {
+		JComboBox<String> comboList = getAnswerSpace();
+		comboList.setSelectedIndex(0);
+	}
+
+}
