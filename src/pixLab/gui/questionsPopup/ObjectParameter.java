62944d8b0a01cb3fc3b56e752307ef94d8423762
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-11-03 21:47:49 -0700

    removed some imports and added one

--- src/pixLab/gui/questionsPopup/ObjectParameter.java
+++ src/pixLab/gui/questionsPopup/ObjectParameter.java
@@ -1,14 +1,11 @@
 package pixLab.gui.questionsPopup;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.*;
 
